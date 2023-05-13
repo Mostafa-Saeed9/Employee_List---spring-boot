e594291008df4aae9ba3baa4427e6243008a498a
Mostafa Saeed <mostafa.saeedx98@gmail.com>
2023-05-13 16:23:52 +0300

    main project fiels

---
+++ src/test/java/com/springBoot/EmpolyeeManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpolyeeManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
